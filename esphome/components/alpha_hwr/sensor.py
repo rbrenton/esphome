a355be6668fbee56a4b3f68ac3bdb52aad626c22
Brenton Strickler <rbrenton@gmail.com>
2025-08-29 21:25:34 -0400

    .

--- esphome/components/alpha_hwr/sensor.py
+++ esphome/components/alpha_hwr/sensor.py
@@ -82,7 +82,6 @@ CONFIG_SCHEMA = (
     .extend(cv.polling_component_schema("60s"))
 )
 
-
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
