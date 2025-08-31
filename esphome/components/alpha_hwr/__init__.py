9533c153838d3fb8bbab4ccef71ddd8fce487baf
Brenton Strickler <rbrenton@gmail.com>
2025-08-31 10:45:22 -0400

    .

--- esphome/components/alpha_hwr/__init__.py
+++ esphome/components/alpha_hwr/__init__.py
@@ -3,7 +3,7 @@ from esphome.components import ble_client
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-CODEOWNERS = ["@jan-hofmeier"]
+CODEOWNERS = ["@rbrenton"]
 
 alpha_hwr_ns = cg.esphome_ns.namespace("alpha_hwr")
 Alpha_HWR = alpha_hwr_ns.class_("Alpha_HWR", ble_client.BLEClientNode, cg.Component)
